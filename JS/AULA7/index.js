a821f4e983b10e1907da824da9e34fae0205481e
MatheusSousa98 <matheusousams.98@gmail.com>
2023-01-25 15:57:31 -0300

    Aula 14

---
+++ JS/AULA7/index.js
@@ -0,0 +1,18 @@
+/**
+ * Não pode-se criar variáveis com palavras reservadas
+ * Variáveis precisam ter nome significativos
+ * Não pode começar o nome de uma constante com um número
+ * Não podem conter espaços ou traços
+ * Utilizamos camelCase
+ * Case-sensitive
+ * Não pode modificar o valor de uma constante
+ * Não utilize var, utilize const
+ */
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero*segundoNumero;
+const resultadoDuplicado = resultado*2;
+
+console.log(primeiroNumero+segundoNumero);
+console.log(typeof(primeiroNumero+segundoNumero));
