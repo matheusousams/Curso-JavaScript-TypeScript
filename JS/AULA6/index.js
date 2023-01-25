9bb4d810a877240ed5a56502253dee59b5c04f3b
MatheusSousa98 <matheusousams.98@gmail.com>
2023-01-25 15:02:32 -0300

    Aula 14

---
+++ JS/AULA6/index.js
@@ -0,0 +1,15 @@
+/**
+ * Não pode-se criar variáveis com palavras reservadas
+ * Variáveis precisam ter nome significativos
+ * Não pode começar o nome de uma variável com um número
+ * Não podem conter espaços ou traços
+ * Utilizamos camelCase
+ * Case-sensitive
+ * Não podemos redeclarar variáveis com let
+ * Não utilize var, utilize let
+ */
+
+let nomeCliente = 'Marcos';
+nomeCliente = 'Paulo';
+
+console.log(nomeCliente);
