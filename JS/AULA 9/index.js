1f4ebe50031064cc7021e12f231f9bb5ccc3cbaa
MatheusSousa98 <matheusousams.98@gmail.com>
2023-02-03 10:18:20 -0300

    Aula 19

--- JS/AULA 9/index.js
+++ JS/AULA 9/index.js
@@ -1,4 +1,16 @@
-var nome = 'Louis';
-var nome = 'Peter';
+const nome = 'Luiz'; // string
+const nome1 = "Luiz"; //string
+const nome2 = `Luiz`; //string
 
-console.log(nome);
+const num1 = 0; //number
+const num2 = 10.52; //number
+let nomeAluno; //undefined -> não aponta para nenhum local na memória
+const sobrenomeAluno = null; // nulo -> não aponta para nenhum local na memória
+const aprovado = false; // boolean = true, false (lógico)
+
+let a = 2;
+const b = a;
+console.log(a,b); // 2, 2
+
+a = 3;
+console.log(a, b); // 3, 2
