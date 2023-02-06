450c4651644a2a525f30d9d09dd373049b904002
MatheusSousa98 <matheusousams.98@gmail.com>
2023-02-06 12:26:30 -0300

    Aula 29

---
+++ JS/AULA 16/index.js
@@ -0,0 +1,27 @@
+const alunos = ['Enzo', 'Valentina', 'Miguel'];
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+//alunos.push('Luiza');
+//alunos.push('Eduardo');
+
+//console.log(alunos.slice(0, -2));
+
+//console.log(alunos[50]);
+
+//delete alunos[1];
+//console.log(alunos[1]);
+
+//const removido = alunos.shift();
+//console.log(removido);
+//console.log(alunos);
+
+//alunos.unshift('Fabio'); //adiciona no começo
+//alunos.unshift('Luiza');
+
+//alunos.push('Gabriela'); //adiciona no fim
+//alunos.push('Valério');
+//alunos.push('Marta');
+
+console.log(alunos);
