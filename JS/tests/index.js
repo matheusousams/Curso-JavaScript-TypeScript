24ec33e13f7dd7a9bd38b84c3a59f5bf122a4598
MatheusSousa98 <matheusousams.98@gmail.com>
2023-02-07 10:35:31 -0300

    Aula 31

---
+++ JS/tests/index.js
@@ -0,0 +1,9 @@
+function multiplicacao(x=1, y=1) {
+    const resultado = x * y;
+    return resultado;
+}
+
+console.log(multiplicacao());
+
+const resultado = multiplicacao(2, 7);
+console.log(resultado);
