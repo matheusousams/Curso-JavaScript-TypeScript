2e4e36cde2371bab339e7e32a54097151f54e808
MatheusSousa98 <matheusousams.98@gmail.com>
2023-02-23 10:45:28 -0300

    Aula 38

---
+++ JS/AULA 22/index.js
@@ -0,0 +1,14 @@
+/**
+ * Operadores Lógicos
+ * && -> and
+ * || -> ou
+ * !  -> not
+ */
+console.log(true && true);
+console.log(false && true);
+
+console.log(true || true);
+console.log(false || true);
+
+console.log(!true || false);
+console.log(!false && true);
