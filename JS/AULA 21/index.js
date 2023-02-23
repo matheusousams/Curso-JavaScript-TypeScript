27da2cbe2f24e1e0e93d66a592301a9266182b5c
MatheusSousa98 <matheusousams.98@gmail.com>
2023-02-23 10:30:09 -0300

    Aula 37

---
+++ JS/AULA 21/index.js
@@ -0,0 +1,15 @@
+/**
+ * Operadores de comparação
+ * > maior que
+ * >= maior ou igual a que
+ * < menor que
+ * <= menor ou igual a que
+ * == igualdade
+ * === igualdade estrita (valor e tipo)
+ * ! diferente (valor)
+ * !== diferente estrito (valor e tipo)
+ */
+
+const num1 = 10;
+const num2 = '10';
+console.log(num1===num2);
