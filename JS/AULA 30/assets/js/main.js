38640b4e6628c0510f263269cbdef77d313dc1dc
MatheusSousa98 <matheusousams.98@gmail.com>
2023-02-27 09:44:16 -0300

    Aula 48

---
+++ JS/AULA 30/assets/js/main.js
@@ -0,0 +1,34 @@
+const h1 = document.querySelector('.container h1');
+const data = new Date();
+h1.innerHTML = data.toString();
+
+function getDiaSemanaTexto(diaSemana) {
+    const diasSemana = ['domingo', 'segunda', 'terça', 'quarta', 'quinta',
+        'sexta', 'sábado'];
+    return diasSemana[diaSemana];
+}
+
+function getNomeMes(numeroMes) {
+    const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maior', 'junho',
+        'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+    return meses[numeroMes];
+}
+
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+function criaDate(data) {
+    const diaSemana = data.getDay();
+    const numeroMes = data.getMonth();
+
+    const nomeDia = getDiaSemanaTexto(diaSemana);
+    const nomeMes = getNomeMes(numeroMes);
+
+    return (
+        `${nomeDia}, ${data.getDate()} de ${nomeMes} ` +
+        `de ${data.getFullYear()} ${zeroAEsquerda(data.getHours())}:${zeroAEsquerda(data.getMinutes())}`
+    );
+}
+
+h1.innerHTML = criaDate(data);
