54bac5cae8a6544facc39dd5e57b8be0ce684e6b
MatheusSousa98 <matheusousams.98@gmail.com>
2023-03-06 11:08:34 -0300

    Aula 64

---
+++ JS/AULA 45/index.js
@@ -0,0 +1,27 @@
+function retornaHora(data) {
+    if(data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instancia de Date');
+    }
+
+    if(!data) {
+        data = new Date();
+    }
+
+    return data.toLocalTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    });
+}
+
+try {
+    const data = new Date('01-01-1970 12:58:12');
+    const hora = retornaHora();
+    console.log(hora);
+} catch(e) {
+    // tratar erro
+    // console.log(e);
+} finally {
+    console.log('tenha um bom dia.');
+}
